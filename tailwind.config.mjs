dc72dde5627a6acc47018b71dd067db94cf2cb0a
Valeria <lerik13@gmail.com>
2025-02-19 13:20:09 -0700

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          50: '#F8F8F8',
+          100: '#ECECEC',
+          200: '#DADADA',
+          300: '#BDBDBD',
+          400: '#9E9E9E',
+          500: '#7E7E7E',
+          600: '#626262',
+          700: '#494949',
+          800: '#323232',
+          900: '#1F1F1F',
+          950: '#121212',
+        },
+        accent: {
+          50: '#FFF7FA',
+          100: '#FFEAF0',
+          200: '#FFD5E1',
+          300: '#FFBACD',
+          400: '#FF9EB8',
+          500: '#FF83A3',
+          600: '#FF678E',
+          700: '#F54D78',
+          800: '#E73564',
+          900: '#D11D50',
+          950: '#BB053A',
+        },
       },
     },
   },
   plugins: [],
-};
+}
